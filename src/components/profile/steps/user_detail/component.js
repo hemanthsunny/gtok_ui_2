1bf64f4e6479bacf5d81235e1dcaeb399075461f
naga_perla <naga@oslr.co.uk>
2021-11-14 22:08:57 +0000

    fix (LG-230): loader updated

--- src/components/profile/steps/user_detail/component.js
+++ src/components/profile/steps/user_detail/component.js
@@ -191,9 +191,14 @@ function Component (props) {
           <div className='text-center'>
             <div className='btn btn-custom col-1 d-none'></div>
             <div className='btn btn-custom col-7 mr-1' onClick={e => relationStatus('cancel_request')}>
-              { isFollowerLoading && loaderComponent() }
-              Request sent &nbsp;
-              <img className='icon-request-sent' src={require('assets/svgs/SentRequest.svg').default} alt="Pending" />
+              {
+                isFollowerLoading
+                  ? loaderComponent()
+                  : <div>
+                    Request sent &nbsp;
+                    <img className='icon-request-sent' src={require('assets/svgs/SentRequest.svg').default} alt="Pending" />
+                  </div>
+              }
             </div>
             <div className='btn btn-custom col-2 ml-1' onClick={msgUser}>
               <img className='icon-search-chat' src={require('assets/svgs/ChatBlack.svg').default} alt="1" />
@@ -204,9 +209,14 @@ function Component (props) {
         !isAdminUser && (follower.status === undefined || follower.status === null) &&
           <div className='text-center'>
             <div className='btn btn-custom col-7 mr-1' onClick={e => relationStatus('follow')}>
-              { isFollowerLoading && loaderComponent() }
-              Follow &nbsp;
-              <img className='icon-request-sent' src={require('assets/svgs/SendRequest.svg').default} alt="Follow" />
+              {
+                isFollowerLoading
+                  ? loaderComponent()
+                  : <div>
+                  Follow &nbsp;
+                  <img className='icon-request-sent' src={require('assets/svgs/SendRequest.svg').default} alt="Follow" />
+                  </div>
+              }
             </div>
             <div className='btn btn-custom col-2 ml-1' onClick={msgUser}>
               <img className='icon-search-chat' src={require('assets/svgs/ChatBlack.svg').default} alt="1" />
